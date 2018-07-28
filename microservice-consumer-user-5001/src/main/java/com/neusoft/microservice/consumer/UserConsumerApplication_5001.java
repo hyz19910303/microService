96b5775b30748c829e07d38eb5bb005ed12044a4
hyz19910303 <565796351@qq.com>
2018-07-28 18:11:51 +0800

    main init

---
+++ microservice-consumer-user-5001/src/main/java/com/neusoft/microservice/consumer/UserConsumerApplication_5001.java
@@ -0,0 +1,25 @@
+package com.neusoft.microservice.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+*Create at 2018年7月28日 下午6:07:51
+*
+*@autuor EVIL
+*
+*@version 1.0
+*
+*ProjectName microservice-consumer-user-5001
+*
+*Description: 
+*        
+*/
+@SpringBootApplication
+public class UserConsumerApplication_5001 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserConsumerApplication_5001.class, args);
+	}
+
+}
