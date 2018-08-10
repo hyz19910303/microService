38cb6f959f03cb59b1cf1dfd69d77e95ed971b45
huyz <565796351@qq.com>
2018-08-10 14:59:31 +0800

    升级到2.0版本

--- microservice-consumer-user-5001/src/main/java/com/neusoft/microservice/consumer/UserConsumerApplication_5001.java
+++ microservice-consumer-user-5001/src/main/java/com/neusoft/microservice/consumer/UserConsumerApplication_5001.java
@@ -3,8 +3,8 @@ package com.neusoft.microservice.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.neusoft.microservice.rule.configuration;
--- microservice-inerfance/src/main/java/com/neusoft/microservice/api/UserInterfance.java
+++ microservice-inerfance/src/main/java/com/neusoft/microservice/api/UserInterfance.java
@@ -2,7 +2,7 @@ package com.neusoft.microservice.api;
 
 import java.util.List;
 
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
